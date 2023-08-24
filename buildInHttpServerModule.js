623bdaf3fec5005c1d6ea79d0c32cb7139cd2c52
Vishal Raj <vishalraj@Vishals-MacBook-Pro.local>
2023-08-24 11:43:15 +0530

    Refactored file name from snake_case to camelCase

--- buildInHttpServerModule.js
+++ buildInHttpServerModule.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 
 const PORT = process.env.PORT || 4000;
 const hostname = "localhost";
-const home = fs.readFileSync("./http_server.html", "utf-8");
+const home = fs.readFileSync("./httpServer.html", "utf-8");
 
 console.log(__dirname);
 
